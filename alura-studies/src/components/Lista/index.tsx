9237285a00ff320be761a8572b9290146f548c4e
Ediovan-Ribeiro <ediovanribeiro.dev@gmail.com>
2025-04-14 06:44:53 -0300

    feat: ajuste nos componentes

--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -1,7 +1,10 @@
 import style from './Lista.module.scss';
 import Item from './Item';
+import { log } from 'console';
+import { useState } from 'react';
 function Lista() {
-  const tarefas = [{
+
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'React',
     tempo: '02:00:00'
   }, {
@@ -10,20 +13,22 @@ function Lista() {
   }, {
     tarefa: "Typescript",
     tempo: "03:00:00"
-  }];
+  }]);
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "estudar estado", tempo: "05:00:00" }])
+      }}> Estudos do dia </h2>
       <ul >
         {tarefas.map((item, index) => (
-         <Item
-         key={index}
-         tarefa={item.tarefa}
-         tempo={item.tempo}
-       /> 
+          <Item
+            key={index}
+            tarefa={item.tarefa}
+            tempo={item.tempo}
+          />
         ))}
       </ul>
-    </aside>
+    </aside >
   )
 }
 
